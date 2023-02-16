588be2dbc0f1be30f29eca7230173287dbd313f7
Cenora <cenora2006@gmail.com>
2023-02-16 19:23:25 -0300

    Init

---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Ela tem um @número com 10 dígitos que identifica exclusivamente a conta bancária.
+ * Ela tem uma cadeia de caracteres que armazena o @nome ou os nomes dos proprietários.
+ * O @saldo pode ser recuperado.
+ * Ela aceita @depósitos.
+ * Ele aceita @saques.
+ * O saldo inicial deve ser positivo.
+ * Os saques não podem resultar em um saldo negativo.
+ */
+
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Classe que representa a conta bancária
+    /// </summary>
+    public class Conta
+    {
+        string Nome { get; set; }
+        int NumConta { get; }
+        decimal Saldo { get; }
+
+        /// <summary>
+        /// Construtor padrão para @nome = "Anônimo"
+        /// </summary>
+        public Conta()
+        {
+            Nome = "Anônimo";
+        }
+
+        /// <summary>
+        /// Método para sacar do saldo
+        /// </summary>
+        /// <param name="valor">Montante</param>
+        /// <param name="data">Quando aconteceu</param>
+        /// <param name="desc">Motivo</param>
+        public void Sacar(decimal valor, DateTime data, string desc){}
+
+        /// <summary>
+        /// Método para depositar em saldo
+        /// </summary>
+        /// <param name="valor">Montante</param>
+        /// <param name="data">Quando</param>
+        /// <param name="desc">Motivo</param>
+        public void Depositar(decimal valor, DateTime data, string desc) {}
+    }
+}
